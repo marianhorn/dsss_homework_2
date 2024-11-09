ae8f25b6e9f410afef8034623d10b2da110c79bb
Marian Horn <marianhorn@gmx.de>
2024-11-09 18:10:15 +0100

    Setup.py changed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2',
+    version='0.1',
+    author='Marian Horn',
+    description='Homework 2 for DSSS course',
+    packages=find_packages(),
+    install_requires=[],
+)
+
